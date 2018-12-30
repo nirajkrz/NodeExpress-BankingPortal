8a84e8959c527bb8d25ac874ce866946acd981dc
Niraj <niraj.kumarz@aol.com>
2018-12-30 11:37:42 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const express = require('express');
 
-const app = express;
+const app = new express();
 
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -12,4 +12,4 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => res.render('index', {title: 'Index'}));
 
-pp.listen(3000, () => { console.log('PS Project Running on port 3000!') });
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
