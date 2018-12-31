7cb4c8ba4ea95730030b33723121092de1e81520
Niraj <niraj.kumarz@aol.com>
2018-12-30 19:49:36 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,10 +18,20 @@ const userData = fs.readFileSync(
     path.join(__dirname, 'json', 'users.json'), 'utf8'
 );
 
+
+// const accountRoutes = require('./routes/accounts.js')
+// const servicesRoutes = require('./routes/services.js')
+
 const users = JSON.parse(userData);
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => res.render('index', {title: 'Index'}));
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+
+// app.use('/account',accountRoutes);
+// app.use('/services',servicesRoutes);
+
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
